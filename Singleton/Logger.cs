d8ef87b065788465548b2844ff2dea4678ce8683
PlamenHP <p.petrov.mail@gmail.com>
2017-01-05 22:23:36 +0000

    initial commit

---
+++ Singleton/Logger.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Singleton
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Logger
+    {
+
+        private List<LogEvent> Events;
+        private static Logger instance;
+        private static object synclock = new object();
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (synclock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Logger();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            this.Events = new List<LogEvent>();
+        }
+
+        public void SaveToLog(string message)
+        {
+            Events.Add(new LogEvent(message));
+        }
+
+        public void PrintLog()
+        {
+            foreach (var ev in Events)
+            {
+                Console.WriteLine($"Time {ev.EventDate.ToShortDateString()}, Event: {ev.Message}");
+            }
+        }
+    }
+}
---
+++ Singleton/Start.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Singleton
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Start
+    {
+        static void Main()
+        {
+            var log = Logger.Instance;
+
+            log.SaveToLog("message 1");
+            log.SaveToLog("message 2");
+
+            var log2 = Logger.Instance;
+
+            log2.SaveToLog("message from log 2");
+
+            log.PrintLog();
+        }
+    }
+}
